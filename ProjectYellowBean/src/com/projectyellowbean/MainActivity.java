f1bb375872fb7ddd2c382ded47a7453926b1ee36
Somshubra Majumdar <titu1994@gmail.com>
2014-06-30 21:37:45 +0530

    Test

--- ProjectYellowBean/src/com/projectyellowbean/MainActivity.java
+++ ProjectYellowBean/src/com/projectyellowbean/MainActivity.java
@@ -1,15 +1,9 @@
 package com.projectyellowbean;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
@@ -17,8 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_main);
-
 		
+		//Dofus does it work
 	}
 
 	@Override
