fa88c9235d96fc58afd688f8f1598be25d390ab7
ishaanj <ishjain321@gmail.com>
2014-07-08 09:58:28 +0530

    Check methods onCreate,onLoadCreate,onLoadfinished check class ContObserver

--- ProjectYellowBean/src/com/projectyellowbean/MainActivity.java
+++ ProjectYellowBean/src/com/projectyellowbean/MainActivity.java
@@ -1,18 +1,40 @@
 package com.projectyellowbean;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.ContentObserver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.projectyellowbean.database.StocksProvider;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+	SimpleCursorAdapter mAdapter; 				
+    CursorLoader cursorLoader;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_main);
 		
+		//Added here
+		String[] columns;
+		Cursor cursor;
+		int[] to;
+		
+		String[] uiBindFrom = {};		
+		int[] uiBindTo = {};
+		mAdapter = new SimpleCursorAdapter(this,R.layout.db_view_main, null, uiBindFrom, uiBindTo,0);  
+	    setListAdapter(mAdapter);
+	    getLoaderManager().initLoader(0, null, this);
 		
 	}
 
@@ -26,9 +48,6 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
@@ -36,6 +55,51 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		
+		CursorLoader cursL=new CursorLoader(this,StocksProvider.CONTENT_URI, null, null, null, null);
+		
+		return null;
+	}
 
+	@Override
+	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
+		
+		Cursor stock = getContentResolver().query(StocksProvider.CONTENT_URI,null,null,null,null);
+		if(stock==null){
+			getContentResolver().registerContentObserver(StocksProvider.CONTENT_URI,true,new ContObserver(null));
+			
+			getLoaderManager().restartLoader(0, null, this);  
+		}
+		else
+		{
+			mAdapter.swapCursor(stock);
+		}
+			
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
+class ContObserver extends ContentObserver{
+
+	public ContObserver(Handler handler) {
+		super(handler);
+		
+	}
+	@Override
+	   public void onChange(boolean selfChange) {
+
+	      this.onChange(selfChange, StocksProvider.CONTENT_URI);
+
+	   }     
+
+	
 }
