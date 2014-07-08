334a1c7e046b6f340212bc05af02830ff5be9f0f
Somshubra Majumdar <titu1994@gmail.com>
2014-07-08 15:40:11 +0530

    Oi. When commiting dont forget to check if all the files are being commited. Else even one missing file and it will not work properly and show errors.
    Anyway, finished the main screen showng the database items.

--- ProjectYellowBean/src/com/projectyellowbean/MainActivity.java
+++ ProjectYellowBean/src/com/projectyellowbean/MainActivity.java
@@ -1,41 +1,55 @@
 package com.projectyellowbean;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.app.LoaderManager;
+import android.app.ProgressDialog;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+//WTF. DO NOT USE ANY SUPPORT LIBRARIES. Whenever it asks ALWAYS SUPPLY THE CORRECT ONE.
+//Eg. This is correct.
+import android.widget.SimpleCursorAdapter;
 
+// THIS IS WRONG. IT WILL CRASH 
+//import android.support.v4.widget.SimpleCursorAdapter;
+
+
+
+import com.projectyellowbean.database.StocksDatabase;
 import com.projectyellowbean.database.StocksProvider;
 
+//								DONT USE LISTACTIVITY. IT SUCKS!
+public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor>{
 
-public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
-
-	SimpleCursorAdapter mAdapter; 				
-    CursorLoader cursorLoader;
+	// Use constants like this to tidy up code. Dont just pass 0 everywhere.
+	private static final int DATA_LOAD = 0;
 	
+	//Cache the listview and the adapter
+	private ListView mListView;
+	private SimpleCursorAdapter mAdapter; 				
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_main);
+
+		mListView = (ListView) findViewById(R.id.stock_list);
 		
-		//Added here
-		String[] columns;
-		Cursor cursor;
-		int[] to;
+		// Made lots of changes here. See what I did.
+		String[] uiBindFrom = new String[] {StocksDatabase.COL_STOCK_NAME, StocksDatabase.COL_STOCK_SYMBOL, StocksDatabase.COL_STOCK_VALUE, StocksDatabase.COL_STOCK_AMOUNT};		
+		int[] uiBindTo = new int[] {R.id.text_stock_name, R.id.text_stock_symbol, R.id.text_stock_value, R.id.text_stock_amount};
 		
-		String[] uiBindFrom = {};		
-		int[] uiBindTo = {};
-		mAdapter = new SimpleCursorAdapter(this,R.layout.db_view_main, null, uiBindFrom, uiBindTo,0);  
-	    setListAdapter(mAdapter);
-	    getLoaderManager().initLoader(0, null, this);
+		mAdapter = new SimpleCursorAdapter(this, R.layout.stock_list_items, null, uiBindFrom, uiBindTo, 0);  
+		mListView.setAdapter(mAdapter);
 		
+		// You forgot to call startLoading(). It won't load otherwise
+		getLoaderManager().initLoader(DATA_LOAD, null, this).startLoading();
 	}
 
 	@Override
@@ -57,49 +71,54 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
 
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		
-		CursorLoader cursL=new CursorLoader(this,StocksProvider.CONTENT_URI, null, null, null, null);
-		
-		return null;
+		// Why did you pass null when the cursor loader was correct ?
+		CursorLoader cursL = new CursorLoader(this, StocksProvider.CONTENT_URI, null, null, null, null);
+		return cursL;
 	}
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
-		
-		Cursor stock = getContentResolver().query(StocksProvider.CONTENT_URI,null,null,null,null);
-		if(stock==null){
-			getContentResolver().registerContentObserver(StocksProvider.CONTENT_URI,true,new ContObserver(null));
-			
-			getLoaderManager().restartLoader(0, null, this);  
+		if(arg1 == null){
+			/**
+			 * Don't make outer classes like that. Android complains about class not found.
+			 * Make inner Anonymous classes like i've done.
+			 * 
+			 * We dont need to be notified about the children. Worry about the parent database only.
+			 * Start up a progress dialog to show that we are doing a lot of work on the database. 
+			 * This loading will happen only once.
+			 */
+			final ProgressDialog pd = ProgressDialog.show(this, "Please wait", "Loading database", true, false);
+			getContentResolver().registerContentObserver(StocksProvider.CONTENT_URI, false, new ContentObserver(new Handler()) {
+
+				@Override
+				public void onChange(boolean selfChange) {
+					super.onChange(selfChange);
+					
+					if(selfChange) {
+						
+						//No need for progress dialog anymore
+						pd.dismiss();
+						// Restart loader like this.
+						getLoaderManager().restartLoader(DATA_LOAD, null, MainActivity.this).startLoading();
+						//We dont need to observe the database anymore now.
+						getContentResolver().unregisterContentObserver(this);
+					}
+				}
+				
+			});
 		}
-		else
-		{
-			mAdapter.swapCursor(stock);
+		else {
+			mAdapter.swapCursor(arg1);
+			mAdapter.notifyDataSetChanged();
 		}
-			
+
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> arg0) {
-		// TODO Auto-generated method stub
+		mAdapter.swapCursor(null);
+		mAdapter.notifyDataSetChanged();
 		
 	}
 
-}
-
-
-class ContObserver extends ContentObserver{
-
-	public ContObserver(Handler handler) {
-		super(handler);
-		
-	}
-	@Override
-	   public void onChange(boolean selfChange) {
-
-	      this.onChange(selfChange, StocksProvider.CONTENT_URI);
-
-	   }     
-
-	
 }
